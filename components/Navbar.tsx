bcedc49d3c339b22d23f8e16f4831ccb864f1abd
devilkiller-ag <ashmitgupta.official@gmail.com>
2023-11-13 18:23:09 +0530

    added theme toggle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import MainNav from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
 
@@ -20,13 +21,14 @@ const Navbar = async () => {
 
   return (
     <div className="border-b ">
-        <div className="flex h-16 items-center px-4">
-            <StoreSwitcher items={stores} />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-                <UserButton afterSignOutUrl="/" />
-            </div>
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
+      </div>
     </div>
   )
 }
